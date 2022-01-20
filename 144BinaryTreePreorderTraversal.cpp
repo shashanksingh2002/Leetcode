b721512cac95b61f0d6dddbb1fc76f9f8da09509
Shashank Singh <82604413+shashanksingh2002@users.noreply.github.com>
2022-01-21 00:26:28 +0530

    Update 144BinaryTreePreorderTraversal.cpp

--- 144BinaryTreePreorderTraversal.cpp
+++ 144BinaryTreePreorderTraversal.cpp
@@ -9,21 +9,11 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+<h1>RECURSIVE SOLUTION </h1>
 class Solution {
 public:
     void check(vector<int> &ans,TreeNode* root){
        if(root==NULL) return;
-      
-       if(root->left==NULL){
-         ans.push_back(root->val);
-         check(ans,root->right);
-         return;
-       }
-       if(root->right==NULL){
-         ans.push_back(root->val);
-         check(ans,root->left);
-         return;
-       }
        ans.push_back(root->val);
        check(ans,root->left);
        check(ans,root->right);
@@ -32,6 +22,29 @@ public:
         vector<int> ans;
        if(root==NULL) return ans;
         check(ans,root);
+      return ans;
+    }
+};
+
+<h1>ITERATIVE SOLUTION </h1>
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+       if(root==NULL) return ans;
+      stack<TreeNode*> s;
+      s.push(root);
+      while(!s.empty()){
+        struct TreeNode* topElement=s.top();
+        ans.push_back(topElement->val);
+        s.pop();
+        if(topElement->right!=NULL){
+          s.push(topElement->right);
+        }
+        if(topElement->left!=NULL){
+          s.push(topElement->left);
+        }
+      }
       return ans;
     }
 };
