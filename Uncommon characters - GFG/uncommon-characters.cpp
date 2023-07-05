3dbc39389180a313a1f9f6c2a0a246f9ca365b34
Shashank Singh <82604413+shashanksingh2002@users.noreply.github.com>
2023-07-06 00:27:06 +0530

    Added solution - LeetHub

---
+++ Uncommon characters - GFG/uncommon-characters.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        string UncommonChars(string A, string B)
+        {
+            string ans = "";
+            int hash1[256]={0};
+            int hash2[256]={0};
+            for(int i=0;i<A.length();i++){
+                hash1[A[i]]++;
+            }
+            
+            for(int i=0;i<B.length();i++){
+                hash2[B[i]]++;
+            }
+            
+            for(int i=0;i<256;i++){
+                if(hash1[i] && !hash2[i] || !hash1[i] && hash2[i]){
+                    char ch = i;
+                    ans += ch;
+                }
+            }
+            return (!ans.length())?"-1":ans;
+        }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string A,B;
+        cin>>A;
+        cin>>B;
+        Solution ob;
+        cout<<ob.UncommonChars(A, B);
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
